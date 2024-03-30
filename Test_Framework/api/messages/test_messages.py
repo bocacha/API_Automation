4e21bd6a6524a6f451774eb558a928dd487469dd
bocacha <martineduardogonzalez@gmail.com>
2024-03-30 18:28:27 -0400

    Adding test to single message

--- Test_Framework/api/messages/test_messages.py
+++ Test_Framework/api/messages/test_messages.py
@@ -17,6 +17,7 @@ load_dotenv()
 email = os.getenv("EMAIL")
 key = os.getenv("KEY")
 
+
 class TestPlaylist:
 
     @classmethod
@@ -25,8 +26,14 @@ class TestPlaylist:
         Setup Class is called before the class is initiated
         """
         cls.conn = http.client.HTTPSConnection("mailsac.com")
-        cls.headers = { 'Mailsac-Key': "k_pZ2lvXIuAkvoXBi4bBm99w7djLFLkEeunR6fImwwd4" }
+        cls.headers = { 'Mailsac-Key': key}
         cls.email = email
+        cls.conn.request("GET", f"/api/addresses/{cls.email}/messages", headers=cls.headers)
+        res = cls.conn.getresponse()
+        data = res.read()
+        #get the message "id" from the response
+        cls.message_id = json.loads(data.decode("utf-8"))[0]["_id"]
+        LOGGER.debug("Message ID: %s", cls.message_id)
 
     def test_get_all_messages(self):
         """
@@ -36,6 +43,20 @@ class TestPlaylist:
         res = self.conn.getresponse()
         data = res.read()
         messages = json.loads(data.decode("utf-8"))
+        #get the message "id" from the response
+        message_id = messages[0]["_id"]
         LOGGER.debug("Response to get all messages: %s", messages)
         assert res.status == 200, "Response code is not 200"
         assert isinstance(messages, list), "Response content is not a list of messages"
+
+    def test_get_message(self):
+        """
+        Test Get Message
+        """
+        self.conn.request("GET", f"/api/addresses/{self.email}/messages/{self.message_id}", headers=self.headers)
+        res = self.conn.getresponse()
+        data = res.read()
+        message = json.loads(data.decode("utf-8"))
+        LOGGER.debug("Response to get message: %s", message)
+        assert res.status == 200, "Response code is not 200"
+        assert isinstance(message, dict), "Response content is not a message"
