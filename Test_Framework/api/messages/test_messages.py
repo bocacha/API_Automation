ae891901b5212b0ca38c9d7286b27956d7cb59b6
bocacha <martineduardogonzalez@gmail.com>
2024-03-31 09:15:03 -0400

    Adding test_create_message test

--- Test_Framework/api/messages/test_messages.py
+++ Test_Framework/api/messages/test_messages.py
@@ -5,6 +5,12 @@ import requests
 import http.client
 import json
 
+import time
+import email.utils
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
 from dotenv import load_dotenv
 from ...utils.logger import get_logger
 
@@ -16,6 +22,17 @@ load_dotenv()
 
 email = os.getenv("EMAIL")
 key = os.getenv("KEY")
+
+API_TOKEN = os.getenv('KEY')
+BASE_URL = 'https://mailsac.com/api/'
+FROM_ADDRESS = os.getenv('FROM_ADDRESS')
+TO_ADDRESS = os.getenv('TO_ADDRESS')
+SUBJECT = 'Mailsac SMTP Validate Email Send'
+SMTP_SERVER = os.getenv('SMTP_SERVER')
+SMTP_PORT = os.getenv('SMTP_PORT')
+SMTP_USERNAME = os.getenv('SMTP_USERNAME')
+SMTP_PASSWORD = os.getenv('SMTP_PASSWORD')
+
 
 
 class TestPlaylist:
@@ -42,9 +59,9 @@ class TestPlaylist:
         res = self.conn.getresponse()
         data = res.read()
         messages = json.loads(data.decode("utf-8"))
-        LOGGER.debug("Response to get all messages: %s", messages)
+        LOGGER.debug("Response to get all messages(Text): %s", messages)
         #show the logger message in JSON format:
-        LOGGER.info("Response to get all messages: %s", json.dumps(messages, indent=2))
+        LOGGER.info("Response to get all messages(JSON): %s", json.dumps(messages, indent=2))
         assert res.status == 200, "Response code is not 200"
         assert isinstance(messages, list), "Response content is not a list of messages"
 
@@ -71,7 +88,6 @@ class TestPlaylist:
         count = json.loads(data.decode("utf-8"))
         LOGGER.debug("Response to count messages: %s", count)
         assert res.status == 200, "Response code is not 200"
-        assert isinstance(count, int), "Response content is not an integer"
         
     def test_move_message(self):
         """
@@ -85,7 +101,60 @@ class TestPlaylist:
         message_sender = message["from"][0]["address"]
         message_subject = message["subject"]
         LOGGER.debug("Response to move message: %s", message)
-        #Describe in the Logger message id, message sender, message subject and to wich folder the message was moved:
         LOGGER.info("The message ID: %s,  received from: %s, with the Subject: %s, was moved to the folder: %s", self.message_id, message_sender, message_subject, folder_moved)
         assert res.status == 200, "Response code is not 200"
         assert isinstance(message, dict), "Response content is not a message"
+
+
+    def test_create_message(self):
+        """
+        Test Create Message
+        """
+        BODY_TEXT = ("Mailsac SMTP Validate Email Send\r\n"
+                     "This email was sent using an OFFICE365 email."
+                     )
+        # Create message container - the correct MIME type is multipart/alternative.
+        msg = MIMEMultipart()
+        msg['From'] = FROM_ADDRESS
+        msg['To'] = TO_ADDRESS
+        msg['Subject'] = SUBJECT
+
+        # Attach body to the email
+        msg.attach(MIMEText(BODY_TEXT, 'plain'))
+
+        # Connect to SMTP server and send email
+        try:
+            server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+            server.starttls()
+            server.login(SMTP_USERNAME, SMTP_PASSWORD)
+            text = msg.as_string()
+            server.sendmail(FROM_ADDRESS, TO_ADDRESS, text)
+            server.quit()
+            print("Email sent successfully!")
+        except Exception as e:
+            print("Error: unable to send email -", e)
+            raise e
+        time.sleep(30)
+
+    @staticmethod
+    def check_received(receive_address, send_address, base_url, headers):
+        api_url = '{0}/addresses/{1}/messages'.format(base_url, receive_address)
+        response = requests.get(api_url, headers=headers)
+        for message in response.json():
+            if message['from'][0]['address'] == send_address:
+                return message['received']
+        return '{0} has not received an email from {1}'.format(receive_address, send_address)
+
+    def test_check_received(self):
+        result = self.check_received('bocacha@mailsac.com', send_address=FROM_ADDRESS,
+                                     base_url=BASE_URL, headers={'Mailsac-Key': API_TOKEN})
+        #Logger must detail the email sender, the message id and the time the email was received:
+
+        LOGGER.info("The email from %s was received at %s", FROM_ADDRESS, result)
+
+        LOGGER.debug("Mail received: %s", result)
+    
+                                        
+
+
+                          
